c7138b534ff5467e72f65946bef27a8441339d26
kamil <kammmilkos@gmail.com>
2019-05-30 20:55:59 +0200

    Implement game over conditions

--- Ship.gd
+++ Ship.gd
@@ -3,9 +3,11 @@ extends RigidBody2D
 ## constants
 var THROTTLE_IMPULSE = 10
 var POSITION_OFFSET = 30
-var FLAME_TIMER_TICK = 0.001
-var MAX_FUEL = 100
+var FLAME_TIMER_TICK = 0.1
+var MAX_FUEL = 10000
 var USE_FUEL = 0.1
+var CRITICAL_VELOCITY = 500
+var CRITICAL_Y_POS = 5
 
 signal fuel_update(value)
 
@@ -19,8 +21,13 @@ var flameAnimations
 var fuel = MAX_FUEL
 var fuel_previous = fuel
 
+# collisions
+var collision_pos_lst
+var previous_velocity
+
 ## functions
 func _ready():
+	previous_velocity = Vector2(0, 0)
 	emit_signal("fuel_update", MAX_FUEL)
 	flames = [$FlameBottom, $FlameLeft, $FlameRight]
 	flameTimers = [Timer.new(), Timer.new(), Timer.new()]
@@ -60,6 +67,14 @@ func updateFlames(showFlames):
 			flame.hide()
 			timer.stop()
 
+func _integrate_forces(state):
+	collision_pos_lst = []
+	var rotation = get_rotation_degrees()
+	var position = get_position()
+	for i in range(state.get_contact_count()):
+		var ship_col_pos = (state.get_contact_local_position(i) - position).rotated(deg2rad(-rotation))
+		collision_pos_lst.append(ship_col_pos)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var rotation = deg2rad(get_rotation_degrees())
@@ -91,3 +106,17 @@ func _process(delta):
 	if (fuel_previous != fuel):
 		fuel_previous = fuel
 		emit_signal("fuel_update", fuel)
+		
+	print(previous_velocity.distance_to(Vector2()))
+	if len(collision_pos_lst) > 0:
+		var collision_min_y = collision_pos_lst[0].y
+		for i in collision_pos_lst:
+			if i.y < collision_min_y:
+				collision_min_y = i.y
+		if collision_min_y < CRITICAL_Y_POS:
+			print('BOOMY!')
+		elif previous_velocity.distance_to(Vector2(0, 0)) > CRITICAL_VELOCITY:
+			print('KBOOM!')
+		
+	if previous_velocity != linear_velocity:
+		previous_velocity = linear_velocity
