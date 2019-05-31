31ce2e715582696b69c23978c848fa20089aca24
kamil <kammmilkos@gmail.com>
2019-05-31 23:57:45 +0200

    Add title screen

---
+++ assets/TitleScreen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	pass # Replace with function body.
+
+#func _process(delta):
+#	pass
+
+
+func _on_NewGame_pressed():
+	print('New Game pressed')
+
+
+func _on_Exit_pressed():
+	print('Exit pressed')
